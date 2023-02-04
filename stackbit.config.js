568d2734df1f92499743e133076fc633788d661d
Stackbit Code Editor <projects@stackbit.com>
2023-02-04 00:58:53 +0000

    stackbit.config.js: changed

--- stackbit.config.js
+++ stackbit.config.js
@@ -119,7 +119,6 @@ export default {
             fields: [localeFieldOverride]
         },
         ImageSection: {
-            fields: [localeFieldOverride]
         },
         MarkdownSection: {
         },
